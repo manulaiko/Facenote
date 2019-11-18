69460526db1539bdbbfb62df372364817ecc6916
manulaiko <manulaiko@gmail.com>
2019-11-18 17:32:17 +0100

    Fix and clean up
     * Move components to /app/components.
     * Fix vertical scrollbar always visible.
     * Download hero background image.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { UserHeroComponent } from './user-hero/user-hero.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { UserHeroComponent } from './components/user-hero/user-hero.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { MyFeedComponent } from './pages/my-feed/my-feed.component';
 import { MyThingsComponent } from './pages/my-things/my-things.component';
 import { MyContactsComponent } from './pages/my-contacts/my-contacts.component';
